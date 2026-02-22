709c7642f8bf8367accc447e0a3ef9650730c017
digu-dev <rodrigo.dev.contact@gmail.com>
2026-02-22 17:07:27 -0300

    fist commit

---
+++ src/test/java/com/digu/dev/TodoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digu.dev.TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
